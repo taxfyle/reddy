5ab50b0369c4f571ee99417326f31133c2ea0c61
Hugo <juicemia@users.noreply.github.com>
2024-05-16 15:41:19 -0400

    Add flag to show progress for memory-usage (#10)
    * Update README with some usage docs
    
    * Add flags to memory-usage for showing progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 use redis::{Commands as RedisCommmands, RedisError};
-use std::process;
+use std::{io::Write, process, thread, time};
 
 #[derive(Parser, Debug)]
 #[clap(version)]
@@ -22,44 +22,61 @@ enum Commands {
         /// Key pattern to use for listing keys
         #[arg(default_value_t = String::from("*"))]
         key_pattern: String,
+
+        /// Show progress as keys are being scanned
+        #[arg(long, default_value_t = false)]
+        show_progress: bool,
+
+        /// Show progress and pause after SCAN_BATCH_SIZE keys have been processed
+        #[arg(short = 'b', long, default_value_t = 1000)]
+        scan_batch_size: u32,
     },
 }
 
 impl Args {
     pub fn run(&self) -> Result<(), RedisError> {
         match &self.cmd {
-            Commands::MemoryUsage { key_pattern } => {
+            Commands::MemoryUsage {
+                key_pattern,
+                show_progress,
+                scan_batch_size,
+            } => {
                 println!("run for {}", key_pattern);
 
                 let client = redis::Client::open(self.url.clone())?;
                 let mut con = client.get_connection()?;
+                let mut count_con = client.get_connection()?;
 
-                let size: Option<u32> = con
-                    .keys::<_, Vec<String>>(key_pattern)?
-                    .into_iter()
-                    .map(|key| {
-                        redis::cmd("MEMORY")
-                            .arg("USAGE")
-                            .arg(&key)
-                            .query::<u32>(&mut con)
-                            .inspect_err(|err| {
-                                println!("got error getting memory usage for key {}: {}", key, err)
-                            })
-                            .unwrap_or(0)
-                    })
-                    .reduce(|acc, e| acc + e);
+                let mut scanned = 0;
+                let mut total_size = 0;
+                for key in con.scan_match::<&str, String>(&key_pattern)? {
+                    let size = redis::cmd("MEMORY")
+                        .arg("USAGE")
+                        .arg(&key)
+                        .query::<u32>(&mut count_con)
+                        .inspect_err(|err| {
+                            println!("error getting memory usage for key {}: {}", key, err);
+                        })
+                        .unwrap_or(0);
 
-                match size {
-                    None => {
-                        println!("got no keys for pattern");
-                        process::exit(1)
+                    total_size = total_size + size;
+
+                    scanned = scanned + 1;
+                    let batch_reached = scanned % scan_batch_size == 0;
+
+                    if *show_progress && batch_reached {
+                        print!("\rscanned keys: {} ({} bytes)", scanned, total_size);
+                        let _ = std::io::stdout().flush();
                     }
 
-                    Some(bytes) => {
-                        println!("got {} bytes", bytes);
-                        process::exit(1);
+                    if batch_reached {
+                        thread::sleep(time::Duration::from_millis(500));
                     }
                 }
+                println!("\nfinished scanning {} keys", scanned); // Start a new line after the scanned keys
+                println!("total size: {} bytes", total_size);
+
+                Ok(())
             }
 
             Commands::Ping => {
